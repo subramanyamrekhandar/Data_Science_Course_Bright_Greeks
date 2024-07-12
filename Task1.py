f26eb7c87bd8420523aa5b186cf3a2154c731dfc
Subramanyam Rekhandar <53898299+subramanyamrekhandar@users.noreply.github.com>
2024-07-12 21:17:26 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,1 +1,62 @@
+#Inputs
+a = 10
+b = 4
 
+a = True
+b = False
+
+#Arthmetic Operators
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+# #Relational Operators (comparison)
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#Logical Operators
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
+
+#Bitwise Operators
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(~b)
+print(a << 4)
+print(a >> 5)
+
+#assignment Operators
+a =+4
+print(a)
+
+
+#identity Operators
+a = [1, 2, 3]
+b = [1,2, 3]
+
+c = a  # c= [1,2,3]
+
+print(a is b)
+print(a is c)
+print(a is not b)
+
+
+
+#Membership Operators
+number = [1, 2, 3, 4]
+
+print(3 in number)
+print(5 in number)
+print(6 not in number)
